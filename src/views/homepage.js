9f5d000b38a6ab8c54f82913e6134c64441ad346
tavismac1 <w0427972@campus.nscc.ca>
2022-07-20 20:50:47 -0300

    fetching data from api and display on home page

--- src/views/homepage.js
+++ src/views/homepage.js
@@ -1,8 +1,37 @@
-const Homepage = () => {
-    return (
-        <div className="homeCon">
-            Welcome to manual maker
+import React, { useEffect, useState } from "react";
 
+const Homepage = () => {
+    const [posts, makePosts] = useState(null) //set up getter and setter for this object
+    useEffect(() => { //on render set our state to our fetched json object so it is always up to date
+        const getPosts = async () => {
+            const res = await fetch('https://guarded-bayou-85189.herokuapp.com/all')
+            const result = await res.json()
+
+            if (res.ok) {
+                makePosts(result)
+                console.log('should get result back')
+            }
+        }
+        getPosts()
+    }, [])
+
+    return ( 
+        <div className="section">
+            {posts && posts.map((x) => (
+                <div className="post" key={x._id}>
+                    <h4 className="user"> user: {x.user} </h4>
+                    <h4 className="title"> title: {x.title} </h4>
+                    <h4 className="category"> category: {x.category} </h4>
+                    <h4> <strong> {x.createdAt} </strong></h4>
+                    <hr className="hr"></hr>
+                    <p> chapter 1 </p>
+                    <div className="chapter"> {x.chapter1} </div>
+                    <hr className="hr"></hr>
+                    <p> chapter 2</p>
+                    <div className="chapter"> {x.chapter2} </div>
+                    <p>{x.chapter3}</p>
+                </div>
+            ))}
         </div>
     )
 }
