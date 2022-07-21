d5aefd8e07868d6d9619cf4b86f026256b4ac87c
tavismac1 <w0427972@campus.nscc.ca>
2022-07-21 03:18:54 -0300

    added deletion of posts

--- src/views/homepage.js
+++ src/views/homepage.js
@@ -5,6 +5,26 @@ const Homepage = () => {
     const [searchInput, setSearch] = useState('')
     const [posts, makePosts] = useState(null) //set up getter and setter for this object
     const [searchErr, setSearchErr] = useState('Search a category')
+    const [deleteID, setID] = useState()
+
+    const test = () => {
+        console.log('DELETING')
+    }
+
+    const deleteManual = async (e) => {
+        e.preventDefault()
+        console.log('DELETE MANUAL')
+        const res = await fetch('https://guarded-bayou-85189.herokuapp.com/delete/'+ deleteID, {
+            method: 'DELETE'
+        })
+        const result = await res.json()
+
+        if (res.ok) {
+            makePosts(result)
+            return
+        }
+        if (!res.ok) setSearchErr('No matches found :(')
+    }
 
     const searchRes = async (e) => { //for category matching
         e.preventDefault()
@@ -36,11 +56,18 @@ const Homepage = () => {
         <div className="section">
             <form onSubmit={searchRes}>
                 <input onChange={(e) => setSearch(e.target.value)} className="searchBar" type="text" placeholder={searchErr}></input>
-                <button className="searchBtn"> | search</button>
+                <button className="searchBtn"> <span class="material-symbols-outlined">
+                pageview
+                </span></button>
             </form>
 
             {posts && posts.map((x) => (
                 <div className="post" key={x._id}>
+                    <form onSubmit={deleteManual}>
+                        <button onClick={ () => setID(x._id)} className="deleteBtn"><span class="material-symbols-outlined">
+                        delete
+                        </span></button>
+                    </form>
                     <h4 className="user"> - user: {x.user} </h4>
                     <h4 className="title"> - title: {x.title} </h4>
                     <h4 className="category"> - category: {x.category} </h4>
